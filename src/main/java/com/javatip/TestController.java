9ec5e9a694eaa471d1c5fec9d4f60660de2257f2
gangadhar <gangadhart444@gmail.com>
2022-07-22 12:54:54 +0530

    Added docker configuration

---
+++ src/main/java/com/javatip/TestController.java
@@ -0,0 +1,13 @@
+package com.javatip;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping("/api/message")
+	public String getMessage() {
+		return "Welcome to ci cd git hub and docker";
+	}
+}
